dd33c251176cebc1d89ad38a3cbdddbc4aafb730
cabart <abartc@student.ethz.ch>
2022-02-22 17:20:25 +0100

    added functionality to server connection, added default test rspec file

--- emulab_experiments/emulabConnection.py
+++ emulab_experiments/emulabConnection.py
@@ -3,15 +3,18 @@
 
 # Note: has to be run with sudo permissions
 
+# General notes for implementer:
+# after initialization only self.cred (user credentials) and self.sliceurn are known
+# self.slice and self.sliver have always to be checked for (if not null or even better if they have expired)
+
+# possible additions:
+# - binding more users to a single slice
+
 from getpass import getpass
+from re import I
 import sys
-import pwd
-import getopt
 import time
 import os
-import re
-import xmlrpc
-import traceback
 import ssl
 
 # server connection libraries
@@ -21,13 +24,15 @@ import xmlrpc.client as xmlrpclib
 import http.client as httplib
 
 from cryptography import x509
+
+# not supported anymore it seems
 from cryptography.x509.oid import NameOID
 from cryptography.x509.oid import ExtensionOID
 
 # Todo: add logging
 import logging
 
-logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+logging.basicConfig(format='%(levelname)s:: %(message)s', level=logging.DEBUG)
 
 
 class InitializeError(Exception):
@@ -39,15 +44,28 @@ class InitializeError(Exception):
 
 class emulabConnection:
     # emulab server
-    xmlrpc_server = {"ch": "www.emulab.net", "sr": "www.emulab.net", "sa": "www.emulab.net"}
-    server_path = {"ch": ":12369/protogeni/xmlrpc/", "sr": ":12370/protogeni/pubxmlrpc/", "sa": ":12369/protogeni/xmlrpc/"}
+    xmlrpc_server = {"ch": "www.emulab.net", "sr": "www.emulab.net", "sa": "www.emulab.net", "cm": "www.emulab.net"}
+    server_path = {"ch": ":12369/protogeni/xmlrpc/", "sr": ":12370/protogeni/pubxmlrpc/", "sa": ":12369/protogeni/xmlrpc/", "cm": ":12369/protogeni/xmlrpc/"}
+
+    slice = None
+    sliver = None
+    manifest = None
 
     # all locations relative to home location, home location has to be an absolute path
-    def __init__(self, user, home_loc, certificate_loc, password_loc, experiment_name="emulab-experiment"):
+    def __init__(self, user, home_loc=None, certificate_loc='.ssl/cloudlab.pem', password_loc='.ssl/password', experiment_name="emulab-experiment"):
         logging.info("Start emulab connection setup")
 
+        if home_loc is None:
+            username = os.environ.get('SUDO_USER', os.environ.get('USERNAME'))
+            self.HOME = os.path.expanduser(f'~{username}')
+            logging.info("No home directory specified. '" + self.HOME + "' is used as home directory now")
+            if self.HOME is None:
+                raise InitializeError("Could not find a username variable in terminal, should specify a home directory")
+        else:
+            self.HOME = home_loc
+            logging.info("Home directory: " + self.HOME)
+
         self.user = user
-        self.HOME = home_loc
         self.certificate_loc = os.path.join(self.HOME,certificate_loc)
         self.password_loc = os.path.join(self.HOME, password_loc)
         self.experiment_name = experiment_name  # = slice name
@@ -97,18 +115,30 @@ class emulabConnection:
             self.password = getpass("Emulab password:")
 
         # get self credential, used for most calls at sa (slice authority)
+        logging.debug("Getting credentials of user")
         cred = self.get_self_credential()
         if cred == -1:
             raise InitializeError("Could not get self credential when setting up emulab connection")
         else:
             self.cred = cred
 
-        logging.info("Successfully setup emulab connection")
+        # get SSH keys
+        params = {}
+        params["credential"] = self.cred
+        rval, response = self.do_method_retry("sa", "GetKeys", params)
+        if rval:
+            raise InitializeError("Could not get ssh keys")
+        else:
+            self.keys = response["value"]
+            print(self.keys)
+
+        logging.info("Successfully setup emulab connection!\n")
 
 
     def do_method(self, module, method, params, version=None):
         if module not in self.xmlrpc_server or module not in self.server_path:
-            logging.error("Invalid call at:",module,"in do_method call")
+            logging.error("Invalid call at: " + module + " in do_method call")
+            raise Exception("Invalid server module")
         else:
             addr = self.xmlrpc_server[module]
             path = self.server_path[module]
@@ -126,6 +156,7 @@ class emulabConnection:
         ctx.set_ciphers("DEFAULT:@SECLEVEL=1")
         ctx.load_cert_chain(self.certificate_loc,password=self.password)
         
+        # TODO: look into this in more detail
         # maybe should remove them if possible
         ctx.check_hostname = False
         ctx.verify_mode = ssl.CERT_NONE
@@ -191,6 +222,7 @@ class emulabConnection:
             rval, response = self.do_method(suffix, method, params)
         return (rval, response)
 
+
     def get_self_credential(self):
         params = {}
         rval, response = self.do_method_retry("sa", "GetCredential", params)
@@ -200,6 +232,7 @@ class emulabConnection:
         else:
             return response["value"]
     
+
     def getVersion(self):
         rval, response = self.do_method("sa", "GetVersion", {})
         if rval:
@@ -209,21 +242,23 @@ class emulabConnection:
             logging.debug("Server version:" + str(response["value"]))
             return response["value"]
 
+
     def lookupSlice(self):
-        
         params = {}
         params["credential"] = self.cred
         params["type"] = "Slice"
-        params["hrn"] = self.experiment_name
+        params["hrn"] = self.experiment_name # or params["urn"] = self.sliceurn
+
         rval, response = self.do_method_retry("sa", "Resolve", params)
-        if rval == 0:
-            logging.info("Slice already exists")
-            #logging.debug(str(response["value"]))
-            return True
-        else:
-            logging.info("Slice does not exist yet")
+        if rval:
+            logging.info("Slice does not exist yet or has expired")
             return False
+        else:
+            logging.info("Slice already exists")
+            logging.debug(str(response["value"]))
+            return True
     
+
     def createSlice(self, duration=4):
         """Create a new slice with slice name given in initialization (experiment_name)
 
@@ -251,16 +286,135 @@ class emulabConnection:
             logging.error(str(response))
             return False # or maybe raise exception
         else:
-            myslice = response["value"]
+            self.slice = response["value"]
             logging.info("Slice successfully created")
             #logging.info("Slice created: " + str(myslice))
             return True
+    
 
+    def getSliceCredential(self):
+        params = {}
+        params["credentials"] = (self.cred,)
+        params["type"] = "Slice"
+        params["urn"] = self.sliceurn
+
+        rval, response = self.do_method_retry("sa", "GetCredential", params)
+        if rval:
+            logging.error("Could not get slice credentials")
+            return False
+        else:
+            logging.debug("Got slice credentials")
+            self.slice = response["value"]
+            return True
+
+    def UpdateSliceInformation(self):
+        # errors should only come up if slice does not exist anymore 
+        if not self.lookupSlice():
+            raise Exception("Slice does not exist")
+        if not self.getSliceCredential():
+            raise Exception("Could not get slice credentials")
+
+
+    def createSliver(self,rspec=None):
+        self.UpdateSliceInformation()
+
+        if rspec is None:
+            path = './default.rspec'
+            try:
+                f = open(path)
+                rspec = f.read()
+                f.close()
+            except IOError:
+                logging.error("Reading of rspec failed. File may not exist?")
+                logging.error("File path: ",path)
+                return False
+
+        params = {}
+        params["credentials"] = (self.slice,)
+        params["slice_urn"] = self.sliceurn
+        params["rspec"] = rspec
+        params["keys"] = self.keys
+
+        print("params:", params)
+
+        rval, response = self.do_method("cm", "CreateSliver", params)
+        if rval:
+            logging.error("Could not create sliver")
+            return False
+        else:
+            logging.debug("Created sliver")
+            self.sliver, self.manifest = response["value"]
+            print("sliver:",self.sliver)
+            print("manifest:",self.manifest)
+            return True
+
+
+    def lookupSliver(self):
+        params = {}
+        params["credentials"] = (self.slice,)
+        params["urn"] = self.sliceurn
+        rval, response = self.do_method("cm", "Resolve", params, version="2.0")
+        if rval:
+            logging.error("Could not resolve slice")
+            return False
+        else:
+            if not "sliver_urn" in response["value"]:
+                logging.error("no sliver found in this slice")
+                return False
+            else:
+                logging.info("Sliver found:")
+                print(response["value"]["sliver_urn"])
+                self.sliverurn = response["value"]["sliver_urn"]
+                return True
+
+
+    def getSliverCredential(self):
+        params = {}
+        params["credentials"] = (self.slice,)
+        params["slice_urn"] = self.sliceurn
+
+        rval, response = self.do_method("cm", "GetSliver", params, version="2.0")
+        if rval:
+            logging.error("Could not get sliver credentials")
+            return False
+        else:
+            logging.debug("Got sliver credentials")
+            self.sliver = response["value"]
+            return True
+    
+
+    def UpdateSliverInformation(self):
+        self.UpdateSliceInformation()
+        # errors should only come up if slice does not exist anymore 
+        if not self.lookupSliver():
+            raise Exception("Sliver does not exist")
+        if not self.getSliverCredential():
+            raise Exception("Could not get Sliver credentials")
+
+
+    def deleteSliver(self):
+        self.UpdateSliverInformation()
+
+        params = {}
+        params["credentials"] = (self.sliver,)
+        params["sliver_urn"] = self.sliverurn
+
+        rval, response = self.do_method("cm", "DeleteSliver", params, version="2.0")
+        if rval:
+            logging.error("Deleting sliver failed")
+            return False
+        else:
+            logging.info("Ticket has been added for remaining time")
+            self.ticket = response["value"]
+            return True
+
+        
 
 
 if __name__ == "__main__":
     try:
-        emuConn = emulabConnection("cabart","/home/cabart",".ssl/encrypted.pem",".ssl/password")
+        #emuConn = emulabConnection("cabart","/home/cabart",".ssl/encrypted.pem",".ssl/password")
+        emuConn = emulabConnection("cabart", certificate_loc=".ssl/encrypted.pem",password_loc=".ssl/password")
     except InitializeError as e:
         logging.error("Emulab initialization failed: " + str(e))
         logging.error("Connection could not get established, abort...")
@@ -287,3 +441,20 @@ if __name__ == "__main__":
     else:
         logging.info("Could not create slice")
 
+    logging.info("Test for sliver creation")
+    worked = emuConn.createSliver()
+    if worked:
+        logging.info("Successfully created sliver")
+    else:
+        logging.info("Could not create sliver")
+
+    # TODO: replace this with status requesting until sliver ready
+    # wait for it to setup
+    time.sleep(120)
+
+    logging.info("Test for sliver deletion")
+    worked = emuConn.deleteSliver()
+    if worked:
+        logging.info("Successfully deleted sliver")
+    else:
+        logging.info("Could not delete sliver")
