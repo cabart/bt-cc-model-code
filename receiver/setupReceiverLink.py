6af72caabd9b6925620dfab80b6c678c26b57da0
cabart <abartc@student.ethz.ch>
2022-03-07 15:02:06 +0100

    modified link setup at receiver

--- receiver/setupReceiverLink.py
+++ receiver/setupReceiverLink.py
@@ -1,5 +1,9 @@
 import yaml
 import subprocess
+import sys
+import argparse
+
+# add/remove outgoing latency from receiver -> switch
 
 def getReceiveriface():
     # get all interfaces (format: '<iface> <ip> <macaddress>')
@@ -13,20 +17,41 @@ def getReceiveriface():
     iface = extendedIfaces[0][0]
     return iface
 
-def main(latency):
-    # add outgoing latency from receiver -> switch
-    iface = getReceiveriface()
+def main():
+    # get arguments
+    parser = argparse.ArgumentParser(description='Add or delete delay at network interface')
+    parser.add_argument('-a', action='store_true', help='add delayed interface')
+    parser.add_argument('-d', action='store_true', help='delete delayed interface')
+    args = parser.parse_args()
 
-    lat = str(latency) + "ms"
-    out = subprocess.check_output(["tc","qdisc","add","dev",iface,"root","netem","delay",lat]).decode("utf-8")
-    return out
-    
-
-if __name__ == "__main__":
+    # get latency from config file
     f = open("/local/config.yaml", "r")
     config = yaml.safe_load(f)
     f.close()
+    latency = config["link_latency"]
 
-    linkLatency = config["link_latency"]
+    # get interface name
+    iface = getReceiveriface()
+    lat = str(latency) + "ms"
 
-    main(linkLatency)
+    if args.a:
+        # add interface
+        try:
+            subprocess.check_output(["sudo","tc","qdisc","add","dev",iface,"root","netem","delay",lat]).decode("utf-8")
+        except subprocess.CalledProcessError as e:
+            # adding failed, most likely because there already is a root qdisc
+            sys.exit(1)
+    elif args.d:
+        # remove interface
+        try:
+            subprocess.check_output(["sudo","tc","qdisc","del","dev",iface,"root","netem"]).decode("utf-8")
+        except subprocess.CalledProcessError as e:
+            # removing failed, most likely because there is no netem qdisc setup
+            sys.exit(1)
+    else:
+        parser.print_help()
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
