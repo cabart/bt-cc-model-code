6d688aeaf7461666ed01d34bcca2cf7c753ba2cd
cabart <abartc@student.ethz.ch>
2022-04-01 17:37:17 +0200

    added more debugging

--- emulab_experiments/remote_scripts/sender_test_bandwidth.py
+++ emulab_experiments/remote_scripts/sender_test_bandwidth.py
@@ -4,14 +4,19 @@ import re
 path = '/local/bandwidth-test.log'
 fout = open(path,'w')
 cmd = 'iperf -c 10.0.0.2 -p 5002 -e -t 10 -f m'
-proc = subprocess.Popen(cmd.split(), stdout=fout)
+proc = subprocess.Popen(cmd.split(), stdout=fout, stderr=fout)
 
 proc.communicate()
+rc = proc.returncode
+fout.write("\nreturn code: " + str(rc))
 fout.close()
 
-f = open(path,'r')
-last = f.readlines()[-1]
-f.close()
+if rc == 0:
+    f = open(path,'r')
+    last = f.readlines()[-1]
+    f.close()
 
-bandwidth = re.findall('(\d+) Mbits/sec', last)[0]
-print("bandwidth:",bandwidth)
+    bandwidth = re.findall('(\d+) Mbits/sec', last)[0]
+    print("bandwidth:",bandwidth)
+else:
+    print("return code: ",str(rc),"0 Mbits/sec")
