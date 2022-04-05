144634c3ed9b4cb459e99aa9fdc3d0d50232abf5
cabart <abartc@student.ethz.ch>
2022-04-05 17:10:33 +0200

    changed RED

--- emulab_experiments/remote_scripts/switch_setup_links.py
+++ emulab_experiments/remote_scripts/switch_setup_links.py
@@ -11,6 +11,17 @@ from remote_lib import remote
 
 import switch_get_ifaces
 
+RED_CORRECTION_FACTOR = {
+    0.5: 83/79,
+    1.0: 83/75,
+    2.0: 83/69,
+    3.0: 83/68,
+    4.0: 83/58,
+    5.0: 83/68, # 78
+    6.0: 83/58,
+    7.0: 83/51  # 57
+}
+
 def addSendersLimits(sending_behavior, logger):
     for host in sending_behavior:
         for hostname,properties in host.items():
@@ -35,23 +46,21 @@ def removeSendersLimits(sending_behavior, logger):
                 logger.error("removing failed, probably no netem set up due to zero latency")
 
 
-def addReceiverLimits(latency, use_red, capacity, queue_length, logger):
+def addReceiverLimits(latency, use_red, capacity, queue_length, max_queue_size, link_size, logger):
     lat = str(latency) + "ms"
     iface = switch_get_ifaces.getReceiveriface()
-    bandwidth = str(capacity) + "mbit"
+    bandwidth = str(capacity) + "Mbit"
     limit = str(queue_length)
 
     logger.info("Add receiver link setup: latency {}, bandwidth {}, queue_size {}, iface {}".format(lat,bandwidth,limit,iface))
     try:
+        subprocess.check_output(["sudo","tc","qdisc","add","dev",iface,"root","handle","5:0","htb","default","1"])
+        subprocess.check_output(["sudo","tc","class","add","dev",iface,"parent","5:0","classid","5:1","htb","rate",bandwidth,"burst","15k"])
+        parent = "5:1"
         if use_red:
-            subprocess.check_output(["sudo","tc","qdisc","add","dev",iface,"root","handle","5:0","htb","default","1"])
-            subprocess.check_output(["sudo","tc","class","add","dev",iface,"parent","5:0","classid","5:1","htb","rate",bandwidth,"burst","15k"])
-            subprocess.check_output(["sudo","tc","qdisc","add","dev",iface,"parent","5:1","handle","6:","red","limit","1000000","min","30000","max","35000","avpkt","1500","burst","20","bandwidth",bandwidth,"probability","1"])
-            subprocess.check_output(["sudo","tc","qdisc","add","dev",iface,"parent","6:","handle","10:","netem","delay",lat,"limit",limit])
-        else:
-            subprocess.check_output(["sudo","tc","qdisc","add","dev",iface,"root","handle","5:0","htb","default","1"])
-            subprocess.check_output(["sudo","tc","class","add","dev",iface,"parent","5:0","classid","5:1","htb","rate",bandwidth,"burst","15k"])
-            subprocess.check_output(["sudo","tc","qdisc","add","dev",iface,"parent","5:1","handle","10:","netem","delay",lat,"limit",limit])
+            subprocess.check_output(["sudo","tc","qdisc","add","dev",iface,"parent","5:1","handle","6:","red","limit",str(3*max_queue_size),"min",str(link_size),"max",str(max_queue_size),"avpkt","1514","burst",str(int((2*link_size+max_queue_size)/3000)+1),"bandwidth",bandwidth,"probability","1"])
+            parent = "6:"
+        subprocess.check_output(["sudo","tc","qdisc","add","dev",iface,"parent",parent,"handle","10:","netem","delay",lat,"limit",limit])
         
     except subprocess.CalledProcessError as e:
         # adding failed, most likely because there already is a root qdisc
@@ -89,6 +98,12 @@ def main():
     use_red = config["use_red"]
 
     queue_length = config["inferred"]["buffer_size"]
+    switch_buffer = config["switch_buffer"]
+    bw_delay_product = config["inferred"]["bw_delay_product"]
+    link_size = bw_delay_product * RED_CORRECTION_FACTOR[switch_buffer] if use_red else bw_delay_product
+    max_queue_size = int(switch_buffer * bw_delay_product) + bw_delay_product
+    link_size = int(link_size*1514)
+    max_queue_size = int(max_queue_size*1514)
 
     if args.a:
         # add interfaces
@@ -97,7 +112,7 @@ def main():
         # add latency for each sender
         if source_latency:
             addSendersLimits(config["sending_behavior"], logger)
-        addReceiverLimits(latency, use_red, capacity, queue_length, logger)
+        addReceiverLimits(latency, use_red, capacity, queue_length, max_queue_size, link_size, logger)
 
     elif args.d:
         logger.info("Remove switch interfaces")
