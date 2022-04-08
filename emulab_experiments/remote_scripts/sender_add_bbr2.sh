22c846cc9b45fe1547fe56623c5e9ffba84672e7
cabart <abartc@student.ethz.ch>
2022-04-08 11:40:15 +0200

    do restart using emulab api

--- emulab_experiments/remote_scripts/sender_add_bbr2.sh
+++ emulab_experiments/remote_scripts/sender_add_bbr2.sh
@@ -2,7 +2,7 @@
 
 # Install liquorix kernel to add bbr2 support
 # https://liquorix.net/ for more information about the kernel
-sudo add-apt-repository -y ppa:damentz/liquorix
-sudo apt update
-sudo apt install -y linux-image-liquorix-amd64 linux-headers-liquorix-amd64
-sudo reboot
+sudo add-apt-repository -y ppa:damentz/liquorix | sudo tee -a /local/node.log
+sudo apt update | sudo tee -a /local/node.log
+sudo apt install -y linux-image-liquorix-amd64 linux-headers-liquorix-amd64 | sudo tee -a /local/node.log
+echo "Done"
